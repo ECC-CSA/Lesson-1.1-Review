e97ce9fdd6009997d22223e44ccc35f563f495dd
William McCauley <william.mccauley@eldonmustangs.org>
2026-09-22 16:36:43 -0500

    created 2 of 5 Activity reviews.

---
+++ gettingStarted/src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        // TODO: Create a house with print and println statements.
+    }
+}
---
+++ variablesAndDataTypes/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // TODO: Create a food menu with Final variables for food items and their prices, and print them out in a formatted way.
+
+        // Final variables for food items and their prices
+        // Hint: Final variables are capitalized and cannot be changed once assigned.
+
+        // print statements to display the food menu
+    }
+}
